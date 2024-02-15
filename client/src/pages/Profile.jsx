2fa69e63e39b6c8e85b01edd3de306cb75426001
vinayreddy24vr <vinayreddyvr24@gmail.com>
2024-02-14 19:53:42 -0500

    upload image functionality small change

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -46,7 +46,7 @@ const Profile = () => {
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
         <input type='file' ref={fileRef} hidden  accept='image/*' onChange={(e)=> setImage(e.target.files[0])}/>
-        <img src={currentUser.profilePicture} alt='profile' className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2'
+        <img src={formData.profilePicture || currentUser.profilePicture} alt='profile' className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2'
         onClick = {()=> fileRef.current.click()}/>
         <p className='text-sm self-center'>
           {imageError?(<span className='text-red-600'>Error Uploading Image (file should be less than 2 MB)</span>): imagePercent >0 && imagePercent<100 ?(<span className='text-slate-700'>{`Uploading : ${imagePercent} %`}</span>): imagePercent === 100 ? (<span className='text-green-700'>Image uploaded succesfully</span>):('')}
